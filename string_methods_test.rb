7c33cec372e4d06fd496feb0971f77a70b60649d
Jackwhitener <jackwhitener1@gmail.com>
2018-10-23 10:30:41 -0400

    Had a realization, what if someone puts an integer, as the input, or the output? normally for the .replace function, it would present an error. I'll do that too. Failing test 3.

--- string_methods_test.rb
+++ string_methods_test.rb
@@ -11,4 +11,9 @@ class My_test < Minitest::Test
 		output = "dinner rolls"
 		assert_equal("dinner rolls", newreplace(input,output))
 	end
+	def test_replace3
+		input = "Four"
+		output = 4
+		assert_equal("Error, this is a string function, integers begone.",newreplace(input,output))
+	end
 end
