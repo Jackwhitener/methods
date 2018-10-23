7aaffe37a727ba25caaf9496495744f9a9f08332
Jackwhitener <jackwhitener1@gmail.com>
2018-10-23 13:53:53 -0400

    Returns the input its given, probably because I told it to, I guess.

--- string_methods_two.rb
+++ string_methods_two.rb
@@ -1,52 +1,40 @@
-# class String
-	def newdowncase(input)
-		count = 0
-		count1 = 0
-		dc_hash.values.each do |x|
-			count +=1
-			# puts "Count #{count}"
-			input.length.times do |y|
-				count1 +=1
-		# 		# puts "Count1 #{count1}"
-		# 		# puts dc_hash[y]
-		# 		puts x
-				if input[y] == dc_hash[x]
-					input << dc_hash[x]
-				end
+
+def newdowncase(input)
+	result = ""
+	dc_hash.values.each do |x|
+		input.length.times do |y|
+			if input[y] == dc_hash[x]
+				result << x
 			end
 		end
-		# puts input
-		# return result
 	end
-
-	# private
-
-	def dc_hash
-		{	"A" => "a",
-			"B" => "b",
-			"C" => "c",
-			"D" => "d",
-			"E" => "e",
-			"F" => "f",
-			"G" => "g",
-			"H" => "h",
-			"I" => "i",
-			"J" => "j",
-			"K" => "k",
-			"L" => "l",
-			"M" => "m",
-			"N" => "n",
-			"O" => "o",
-			"P" => "p",
-			"Q" => "q",
-			"R" => "r",
-			"S" => "s",
-			"T" => "t",
-			"U" => "u",
-			"V" => "v",
-			"W" => "w",
-			"X" => "y",
-			"Z" => "z"}
-	end
-# end
+	return input
+end
+def dc_hash
+	{	"A" => "a",
+		"B" => "b",
+		"C" => "c",
+		"D" => "d",
+		"E" => "e",
+		"F" => "f",
+		"G" => "g",
+		"H" => "h",
+		"I" => "i",
+		"J" => "j",
+		"K" => "k",
+		"L" => "l",
+		"M" => "m",
+		"N" => "n",
+		"O" => "o",
+		"P" => "p",
+		"Q" => "q",
+		"R" => "r",
+		"S" => "s",
+		"T" => "t",
+		"U" => "u",
+		"V" => "v",
+		"W" => "w",
+		"X" => "y",
+		"Z" => "z"}
+end
 newdowncase("W")
