86c7fd80d37e709a1273412e1680506ff17a7b8c
Jackwhitener <jackwhitener1@gmail.com>
2018-10-23 10:35:23 -0400

    Makes the code file actually put the output onto the screen, telling us when the error occurs.

--- string_methods.rb
+++ string_methods.rb
@@ -1,8 +1,10 @@
 def newreplace(input,output)
 	if input.class != String
 		output = "Error, this is a string function, integers begone."
+		puts output
 	elsif output.class != String
 		output = "Error, this is a string function, integers begone."
+		puts output
 	else
 		input = output
 	end
