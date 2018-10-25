8383add89cafd8b1c5ca7f4e6a36d87c2dd67685
Jackwhitener <jackwhitener1@gmail.com>
2018-10-25 13:08:35 -0400

    Test now passes, returning an error message if input isn't an array

--- array_methods.rb
+++ array_methods.rb
@@ -1,9 +1,13 @@
 def newtake(input, taking)
 	cnt = 0
 	output = []
+	if input.class != Array
+		return "Error, not an array"
+	else
 		taking.times do
 			output << input[cnt]
 			cnt += 1
 		end
-	return output
+		return output
+	end
 end
