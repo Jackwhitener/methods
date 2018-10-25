b8dfba7d5075522e6ccc62d581bf2301945548eb
Jackwhitener <jackwhitener1@gmail.com>
2018-10-25 14:15:24 -0400

    Fix for integer test.

--- array_methods_two.rb
+++ array_methods_two.rb
@@ -1,5 +1,7 @@
 def newdrop(input,dropping)
 	count = 0
+	if dropping.class != Integer
+		return "Generic Error Message"
 	if input.class != Array
 		return "Generic Error Message"
 	else
@@ -7,5 +9,6 @@ def newdrop(input,dropping)
 	puts "Input #{input}"
 	end
 return input
+end
 end
 end
