c4ef3769297c217b1324c8146a9c85beebb7e800
Jackwhitener <jackwhitener1@gmail.com>
2018-10-25 09:23:52 -0400

    test that makes sure 1 = 1

--- string_methods_test.rb
+++ string_methods_test.rb
@@ -2,6 +2,9 @@ require "minitest/autorun"
 require_relative "string_methods.rb"
 require_relative "string_methods_two.rb"
 class My_test < Minitest::Test
+	def test_1
+		assert_equal(1,1)
+	end
 	def test_replace1
 		input = "Strongstring"
 		output = "Wrongstring"
