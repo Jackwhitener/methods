a89366c895a59f7342f6ab2d1f3933304b1a9213
Jackwhitener <jackwhitener1@gmail.com>
2018-10-25 09:05:55 -0400

    ''

--- string_methods.rb
+++ string_methods.rb
@@ -1,10 +1,10 @@
 def newreplace(input,output)
 	if input.class != String
-		# output = "Error, this is a string function, others begone."
-		puts output
+		output = "Error, this is a string function, others begone."
+		# puts output
 	elsif output.class != String
-		# output = "Error, this is a string function, others begone."
-		puts output
+		output = "Error, this is a string function, others begone."
+		# puts output
 	else
 		input = output
 	end
